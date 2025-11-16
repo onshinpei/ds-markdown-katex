7cfab20d623f92c8577d6a8ebdcc3b77d35e64a5
onshinpei <244221001@qq.com>
2025-11-16 21:51:19 +0800

    Update ds-markdown dependency to version 1.0.0 and add react-markdown-typer dependency. Remove unused ds-markdown CSS import in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { katexPlugin } from 'ds-markdown/plugins';
 import type { MarkdownRef } from 'ds-markdown';
 import dataJson from './data.json';
 
-import 'ds-markdown/style.css';
 import 'ds-markdown/katex.css';
 
 function throttle(fn: (...args: any[]) => void, delay: number) {
